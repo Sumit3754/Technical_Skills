c82544ae1f990a4e670780b32fc42830928bff31
Sumit Tech <161143974+Sumit3754@users.noreply.github.com>
2026-01-22 11:55:25 +0530

    Add files via upload
    lec_1

---
+++ Array.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] args){
+        int [] arr = {-8, 0, 6, 4, 2, -5};
+        int max = arr[0];
+        for(int i = 1; i<arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            if(i == arr.length - 1){
+                System.out.println("Largest element is: " + max);
+            }
+            }
+        }    
+    }
+}
+
+// Given an array count no. of elements 
+// having at least 1 element greater than itself --
+
+
